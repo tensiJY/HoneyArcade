2ddad15a28049abf11ac419dec3af79393d8d90d
wndud <wndud@DESKTOP-QSBP7MM>
2021-08-14 19:48:01 +0900

    19:47

--- src/main/java/honeyarcade/login/CustomUserDetails.java
+++ src/main/java/honeyarcade/login/CustomUserDetails.java
@@ -36,8 +36,6 @@ public class CustomUserDetails extends User{
 
 }
 
-
-
 //public User(String username, String password, boolean enabled, boolean accountNonExpired,
 //		boolean credentialsNonExpired, boolean accountNonLocked,
 //		Collection<? extends GrantedAuthority> authorities) {
